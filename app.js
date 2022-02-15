e312ac1842bb90feddbd6a70b25828c8e8ab6388
Matt Pityo <mpityo@gmail.com>
2022-02-14 20:54:56 -0500

    add js file

---
+++ app.js
@@ -0,0 +1,7 @@
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const printProfileData = (profileDataArr) => {
+    console.log(profileDataArgs);
+};
+
+printProfileData(profileDataArgs);
